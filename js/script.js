bed07b78cb474546e246fcbdced74b47110c0b49
Michmol13 <159731985+Michmol13@users.noreply.github.com>
2025-02-20 21:43:07 -0600

    commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
+const navbarLinks = document.querySelectorAll(".nav-link");
+const navbarToggler = document.querySelector(".navbar-toggler");
+const botonAbrirModal = document.getElementById("abrirModal");
+const modalElement = document.getElementById("miModal");
+const carruselElement = document.getElementById("miCarrusel");
+
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Página cargada correctamente");
 
-    // Activar el Modal de Bootstrap manualmente
-    var botonAbrirModal = document.getElementById("abrirModal");
-    var modalElement = document.getElementById("miModal");
-
+    
     if (botonAbrirModal && modalElement) {
         var modal = new bootstrap.Modal(modalElement);
         botonAbrirModal.addEventListener("click", function() {
             modal.show();
         });
     }
-
-    // Controlar el Carrusel de Bootstrap
-    var carruselElement = document.getElementById("miCarrusel");
 
     if (carruselElement) {
         var carrusel = new bootstrap.Carousel(carruselElement, {
@@ -22,14 +22,10 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    // Cerrar el menú de navegación al hacer clic en un enlace (para móviles)
-    var navbarLinks = document.querySelectorAll(".nav-link");
-    var navbarToggler = document.querySelector(".navbar-toggler");
-
     navbarLinks.forEach(function(link) {
         link.addEventListener("click", function() {
-            if (window.innerWidth < 992) { // Solo en pantallas pequeñas
-                navbarToggler.click(); // Cierra el menú
+            if (window.innerWidth < 992) { 
+                navbarToggler.click();
             }
         });
     });
